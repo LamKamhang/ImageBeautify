d117e06eea04a47285771257fcc4d571ce6c6200
LamKamhang <Cool_Lam@outlook.com>
2018-07-12 15:30:56 +0800

    first test

---
+++ build.sh
@@ -0,0 +1,4 @@
+cmake . -Bbuild -G"MinGW Makefiles"
+cd build
+make
+cd ..
---
+++ code/testCode.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namesapce std;
+
+int main(void)
+{
+	cout << "HuangWenXiang: ";
+	cout << "Hello world!" << endl;
+}
