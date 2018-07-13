0ad3d4ebd1d21c664557c18b2a4b99c09f026fcb
LamKamhang <Cool_Lam@outlook.com>
2018-07-13 15:34:59 +0800

    测试qmake

--- build.ps1
+++
@@ -1,6 +0,0 @@
-cmake . -Bbuild -G"MinGW Makefiles"
-cmake . -Bbuild -G"MinGW Makefiles"
-cd build
-mingw32-make
-cd ..
-./build/RBFA.exe
---
+++ build/CMakeFiles/helloworld_autogen.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "helloworld_autogen"
+  "CMakeFiles/helloworld_autogen.dir/AutogenOldSettings.cmake"
+  "CMakeFiles/helloworld_autogen"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/helloworld_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
