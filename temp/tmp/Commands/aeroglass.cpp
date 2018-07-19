ecf73ae29994da514344d5a094c4bf4c470f1846
LamKamhang <cool_lam@outlook.com>
2018-07-19 15:11:24 +0800

    修改目录层次, MVVM / MVC

--- temp/tmp/Commands/aeroglass.cpp
+++
@@ -1,9 +0,0 @@
-#include "aeroglass.h"
-#include "ViewModel/viewmodel.h"
-#include "Commands/command.h"
-#include <QDebug>
-#include <string>
-
-void AeroGlassCommand::exec(){
-    viewmodel->exec_Aero_Glass_command();
-}
